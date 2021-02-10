e9033d3b832763a25c20041b7cb37f74d257899d
AndreaJCharlesSantos <56329185+AndreaJCharlesSantos@users.noreply.github.com>
2021-02-10 11:52:39 -0600

    Add files via upload

---
+++ Final.py
@@ -0,0 +1,61 @@
+class Planta(object):
+	def __init__(self = "", nombre = "",especie = "",temporada = ""):
+		self._nombre = nombre
+		self._especie = especie
+		self._temporada = temporada
+	def getnombre(self):
+		return(self._nombre)
+	def getespecie(self):
+		return(self._especie)
+	def gettemporada(self):
+		return(self._temporada)
+
+	def settemporada(self):
+		return(self._temporada)
+class Jardin(Planta):
+	def __init__(self,numplantas = 0, emplazamiento = "", lugar = "", nombre = "", j = None):
+		#self.emplazamiento = emplazamiento
+		#self.lugar = lugar
+		#self.numplantas = numplantas
+		emplazamiento = lugar
+		numplantas = 0		
+		p = []
+		i = 0
+		for i in p:
+			p[i] = None
+			#p.remove(i)
+
+	def Jardin(self, j):
+		emplazamiento = j.emplazamiento
+		numplantas = j.numplantas
+		for i in range(0,100):
+			if j.p[i] != None:
+				p[i] = Planta(j.p[i].getnombre(),j.p[i].getespecie(),j.p[i].gettemporada())
+			else:
+				p[i] = None
+
+	def Plantar(n = "", esp = "", tiempo = ""):
+		if numplantas >= range(0,100):
+			print("No caben más plantas en el jardín")
+		else:
+			lp = Planta(n, esp)
+			lp.settemporada(tiempo)
+			for i in range(0,100):
+				if p[i] == None:
+					p[i] = lp
+					break
+			numplantas += 1
+
+	def Arrancar(nombre= "", esp = ""):
+		if numplantas == 0:
+			print("No quedan plantas en el jardín")
+		else:
+			for i in range(0,100):
+				if nombre == p[i].getnombre() and esp == p[i].getespecie():
+					p[i].remove()
+					p[i] = None
+					numplantas -= 1
+					break
+#Inicio pedorro >:C
+Prueba = Jardin()
+Prueba.Jardin(100)
